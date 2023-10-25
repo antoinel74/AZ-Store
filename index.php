4ea72bc4df1572968cc8193c8485af4792e42d69
Rosa <rosalie.boulard@gmail.com>
2023-10-25 15:21:36 +0200

    CSS not responsive

--- index.php
+++ index.php
@@ -29,9 +29,10 @@
 
         <section class="marketing">
             <img class="shoe" src="./assets/img/shoe_two.png">
-            <h2>We provide you the best quality</h2>
+            <h2 class="provide">We provide you the <span class="blue">best</span> quality.</h2>
             <p class="text-marketing">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat impedit labore autem, quo adipisci cupiditate magnam modi minima voluptate dolore et quisquam, omnis provident laudantium maiores ad non, dicta minus.</p>
         </section>
+
         <section class="opinions">
             <?php
                 require "./opinion.php";
--- opinion.php
+++ opinion.php
@@ -4,19 +4,19 @@
             "name"=>"Emily from XYZ",
             "from"=>"XYZ",
             "img"=>"assets/img/image-emily.jpg",
-            "review"=>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque enim illum natus, hic laudantium reiciendis amet fugit doloribus odit cum adipisci, aspernatur fuga harum repellendus neque dignissimos sequi delectus in.",
+            "review"=>'"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque enim illum natus, hic laudantium reiciendis amet fugit doloribus odit cum adipisci, aspernatur fuga harum repellendus neque dignissimos sequi delectus in."',
         ],
         [
             "name"=>"Thomas",
             "from" => "corporate",
             "img"=>"assets/img/image-thomas.jpg",
-            "review"=>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque enim illum natus, hic laudantium reiciendis amet fugit doloribus odit cum adipisci, aspernatur fuga harum repellendus neque dignissimos sequi delectus in.",
+            "review"=>'"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque enim illum natus, hic laudantium reiciendis amet fugit doloribus odit cum adipisci, aspernatur fuga harum repellendus neque dignissimos sequi delectus in."',
         ],
         [
             "name"=>"Jennie",
             "from"=>"Nike",
             "img"=>"assets/img/image-jennie.jpg",
-            "review"=>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque enim illum natus, hic laudantium reiciendis amet fugit doloribus odit cum adipisci, aspernatur fuga harum repellendus neque dignissimos sequi delectus in.",
+            "review"=>'"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque enim illum natus, hic laudantium reiciendis amet fugit doloribus odit cum adipisci, aspernatur fuga harum repellendus neque dignissimos sequi delectus in."',
         ]
     ]
 ?>
