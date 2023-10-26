80a9c668139089a2437f6a3800e2071a48dff62e
antoinel74 <antoine.lansman@gmail.com>
2023-10-26 16:59:07 +0200

    fix font-sizes

--- contact.view.php
+++ contact.view.php
@@ -1,9 +1,9 @@
 <hmtl>
     <?php
-        require "./views/partials/head.php";
-        require "./views/partials/nav.php";
-        require "./components/contact.php";
-        require "./views/partials/footer.php";
+    require "./views/partials/head.php";
+    require "./views/partials/nav.php";
+    require "./components/contact.php";
+    require "./views/partials/footer.php";
     ?>
-</html>
 
+    </html>
