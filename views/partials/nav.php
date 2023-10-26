28dd11a8dcd804d7b149ee439f40ffb14cef9d2b
antoinel74 <antoine.lansman@gmail.com>
2023-10-26 09:14:51 +0200

    head done

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,24 +1,25 @@
 <html>
-    <link rel="stylesheet" href="/assets/style/style.css" type="text/css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        @import url("https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200;0,6..12,300;0,6..12,400;0,6..12,500;0,6..12,600;0,6..12,700;0,6..12,800;0,6..12,900;0,6..12,1000;1,6..12,200;1,6..12,300;1,6..12,400;1,6..12,500;1,6..12,600;1,6..12,700;1,6..12,800;1,6..12,900;1,6..12,1000&display=swap");
-    </style>
-    <header>
-        <nav class="topnav">
-            <div class="store">
-                <li><a href="#store">AZ[store]</a></li>
-            </div>
-            <div class="homeAbout">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#products">Products</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </div>
-            <div class="login">
-                <li><a href="/views/cart.view.php"><img class ="cart" src="/assets/img/shopping-cart.svg" alt='shoppingCart'></li>
-                <li><a href="/views/checkout.view.php">Login</a></li>                  
-            </div>
-        </nav>
-    </header>
+<link rel="stylesheet" href="../../assets/style/style.css" type="text/css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200;0,6..12,300;0,6..12,400;0,6..12,500;0,6..12,600;0,6..12,700;0,6..12,800;0,6..12,900;0,6..12,1000;1,6..12,200;1,6..12,300;1,6..12,400;1,6..12,500;1,6..12,600;1,6..12,700;1,6..12,800;1,6..12,900;1,6..12,1000&display=swap");
+</style>
+<header>
+    <nav class="topnav">
+        <div class="store">
+            <li><a href="#store">AZ[store]</a></li>
+        </div>
+        <div class="homeAbout">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#products">Products</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </div>
+        <div class="login">
+            <li><a href="/views/cart.view.php"><img class="cart" src="/assets/img/shopping-cart.svg" alt='shoppingCart'></li>
+            <li><a href="/views/checkout.view.php">Login</a></li>
+        </div>
+    </nav>
+</header>
+
 </html>
