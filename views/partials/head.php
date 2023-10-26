66ad514815d1d3eda2565343014305143b96172d
antoinel74 <antoine.lansman@gmail.com>
2023-10-26 09:19:37 +0200

    style

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
-<link rel="stylesheet" href="../../assets/style/style.css" type="text/css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>AZ Store - Smash The ground !</title>
 <meta name="keywords" content="shoes, sneakers, urban eshop, nike" />
 <meta name="description" content="Basic e-commerce page for a sneaker shop" />
 <meta name="author" content="Antoine LANSMAN, Rosalie BOULARD, Okly IRADUKUNDA" />
+<title>AZ Store - Smash The ground !</title>
+<link rel="stylesheet" href="../../assets/style/style.css" type="text/css">
