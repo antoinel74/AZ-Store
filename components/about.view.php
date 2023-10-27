f382f1906587192e2c7bafa86f8f24add7b33be6
Rosa <rosalie.boulard@gmail.com>
2023-10-27 10:06:07 +0200

    fixing bugs and links

--- components/about.view.php
+++ components/about.view.php
@@ -1,10 +1,11 @@
+<html>
 <?php
     require __DIR__ . "/../views/partials/head.php";
     require __DIR__ .  "/../views/partials/nav.php";
     require __DIR__ . "/../components/about.php";
     echo "<div class='reviews'>
             <h2>Check our reviews</h2>";
-                require __DIR__ . "/components/carousel.php";
+                require "carousel.php";
                 echo "</div>";
     require __DIR__ . "/../views/partials/footer.php";
 ?>
