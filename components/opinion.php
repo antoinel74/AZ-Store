0325fb76ff0b0912ef0ad1fd7fdebe337ff92382
antoinel74 <antoine.lansman@gmail.com>
2023-10-27 11:14:37 +0200

    fix spacing

--- components/opinion.php
+++ components/opinion.php
@@ -4,18 +4,18 @@ $opinions = [
         "name" => "Emily",
         "from" => "XYZ",
         "img" => "../assets/img/image-emily.jpg",
-        "review" => '"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque enim illum natus, hic laudantium reiciendis amet fugit doloribus odit cum adipisci, aspernatur fuga harum repellendus neque dignissimos sequi delectus in."',
+        "review" => '"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque enim illum natus, hic laudantium reiciendis amet fugit doloribus odit cum adipisci, aspernatur fuga harum repellendus."',
     ],
     [
         "name" => "Thomas",
-        "from" => "corporate",
+        "from" => "SneakerWorld",
         "img" => "../assets/img/image-thomas.jpg",
-        "review" => '"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque enim illum natus, hic laudantium reiciendis amet fugit doloribus odit cum adipisci, aspernatur fuga harum repellendus neque dignissimos sequi delectus in."',
+        "review" => '"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque enim illum natus, hic laudantium reiciendis amet fugit doloribus odit cum adipisci, aspernatur fuga harum repellendus."',
     ],
     [
         "name" => "Jennie",
         "from" => "Nike",
         "img" => "../assets/img/image-jennie.jpg",
-        "review" => '"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque enim illum natus, hic laudantium reiciendis amet fugit doloribus odit cum adipisci, aspernatur fuga harum repellendus neque dignissimos sequi delectus in."',
+        "review" => '"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque enim illum natus, hic laudantium reiciendis amet fugit doloribus odit cum adipisci, aspernatur fuga harum repellendus."',
     ]
 ];
