463854c8a8e188dddd5ebbb69844bf3da5f5cf13
antoinel74 <antoine.lansman@gmail.com>
2023-10-27 10:06:01 +0200

    fix path opinions

--- components/about.view.php
+++ components/about.view.php
@@ -1,11 +1,12 @@
 <?php
-    require __DIR__ . "/../views/partials/head.php";
-    require __DIR__ .  "/../views/partials/nav.php";
-    require __DIR__ . "/../components/about.php";
-    echo "<div class='reviews'>
+require __DIR__ . "/../views/partials/head.php";
+require __DIR__ .  "/../views/partials/nav.php";
+require __DIR__ . "/../components/about.php";
+echo "<div class='reviews'>
             <h2>Check our reviews</h2>";
-                require __DIR__ . "/components/carousel.php";
-                echo "</div>";
-    require __DIR__ . "/../views/partials/footer.php";
+require __DIR__ . "/../components/carousel.php";
+echo "</div>";
+require __DIR__ . "/../views/partials/footer.php";
 ?>
+
 </html>
