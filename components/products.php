f30195bf15e9dd5aa2f6bb9178a959400869e1dd
antoinel74 <antoine.lansman@gmail.com>
2023-10-27 10:29:42 +0200

    fix add to cart

--- components/products.php
+++ components/products.php
@@ -36,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
 
         $_SESSION['cart'][] = $productId;
-        header('Location: index.php');
+        /*         header('Location: index.php'); */
     }
 }
 ?>
